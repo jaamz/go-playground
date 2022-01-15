c8525e4c3a3fe9bef6973bd8a3a91203e0d3559e
James Park <jamestkd918@gmail.com>
2022-01-14 18:32:12 -0800

    test command set up for !nobu

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "os/signal"
-    "strings"
-    "syscall"
-    "github.com/bwmarrin/discordgo"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"github.com/bwmarrin/discordgo"
 )
 
-var (
-	Token string
-)
-
-const KuteGoAPIURL = "https://kutego-api-xxxxx-ew.a.run.app"
+var Token string
 
 func init() {
     flag.StringVar(&Token, "t", "", "Bot Token")
@@ -26,6 +18,7 @@ func init() {
 
 func main() {
 
+	// creating new session with token
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session, ", err)
@@ -36,6 +29,7 @@ func main() {
 
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
+	// websocket connection
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("Error opening connection,", err)
@@ -51,8 +45,13 @@ func main() {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
+
+	if m.Content == "!nobu" {
+		const returnNobu string = "I am Nobu Bot!"
+		s.ChannelMessageSend(m.ChannelID, returnNobu)
+	}
 }
+
